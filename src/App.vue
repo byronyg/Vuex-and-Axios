3f88149f8beff9a15e032e7d805fcf07ff6cabf8
Byron Yego <byronyego@gmail.com>
2021-03-11 12:44:17 +0300

    Added Todo Component and Vuex Store

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    <Todos />
+    <div class="container">
+
+      <Todos />
+    </div>
+    
   </div>
 </template>
 
@@ -8,7 +12,7 @@
 import Todos from './components/Todos.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
     Todos
   }
@@ -16,5 +20,15 @@ export default {
 </script>
 
 <style>
-
+body {
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  line-height: 1.6;
+  background: #e8f7f0;
+}
+.container {
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
+}
 </style>
